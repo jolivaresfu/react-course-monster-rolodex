5342cdbb5403a7f44f70aa9e76820d5ca1d58a1d
Juan Olivares <ext_jolivaresfu@sodimac.cl>
2019-12-03 10:11:40 -0300

    change robots size to 150x180

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ export const Card = (props) => (
     <div className='card-container'>
         <img
             alt="monster"
-            src={`https://robohash.org/${props.monster.id}?set=set1`}
+            src={`https://robohash.org/${props.monster.id}?set=set1&size=150x180`}
         />
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
